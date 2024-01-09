47c0d98938864b764a0242537dbd9bb9104cd1a1
nitrogency <nitrog@protonmail.com>
2024-01-09 18:36:36 +0200

    Fixed 404 page

--- frontend/static/js/views/None.js
+++ frontend/static/js/views/None.js
@@ -8,8 +8,8 @@ export default class extends AbstractView {
 
     async getHtml(){
         return `
-            <body class="min-h-screen bg-gray-900">
-                <div class="flex flex-col items-center justify-start py-8 h-screen max-w-screen-xl">
+            <body>
+                <div class="flex flex-col items-center justify-start py-8 h-screen">
                     <h1 class="text-5xl font-extrabold dark:text-white pb-8">The page you're looking for wasn't found!</h1>
                     <p class="text-lg font-normal lg:text-xl">Click <a href="/" class="nav__link text-yellow-300 hover:underline" data-link>here</a> to go back.</p>
                 </div>  
