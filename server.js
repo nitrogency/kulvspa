6ef05bda0614b78bd8ea5282ff33d826439e8f40
nitrogency <nitrog@protonmail.com>
2024-01-13 10:06:01 +0200

    Changed port

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 443, () => {
     console.log("Server started");
 })
