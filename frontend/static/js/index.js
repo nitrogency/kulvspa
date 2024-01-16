accba0d3ab420da9b8d6a00d0ad0c87b395b1c6b
nitrogency <nitrog@protonmail.com>
2024-01-16 14:07:17 +0200

    Localization support

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -5,6 +5,12 @@ import About from "./views/About.js";
 import Guides from "./views/Guides.js";
 import None from "./views/None.js";
 
+const defaultLocale = "en";
+
+let locale;
+
+let translations = {};
+
 const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
 
 const getParams = match => {
@@ -14,12 +20,13 @@ const getParams = match => {
     return Object.fromEntries(keys.map((key, i) => {
         return [key, values[i]];
     }));
-}
+};
 
 const navigateTo = (url) => {
     history.pushState(null, null, url);
     router();
-}
+};
+
 const router = async () => {
     const routes = [
         { path: "/none", view: None },
@@ -28,7 +35,6 @@ const router = async () => {
         { path: "/posts/:id", view: PostView },
         { path: "/guides", view: Guides },
         { path: "/about", view: About },
-
     ];
 
     const potentialMatches = routes.map(route => {
@@ -49,16 +55,46 @@ const router = async () => {
 
     const view = new match.route.view(getParams(match));
     document.querySelector("#app").innerHTML = await view.getHtml();
+    translatePage();
 };
 
 window.addEventListener("popstate", router);
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     document.body.addEventListener("click", e => {
         if (e.target.matches("[data-link]")) {
             e.preventDefault();
             navigateTo(e.target.href);
         }
     });
+    
+    await setLocale(defaultLocale);
     router();
 });
+
+async function setLocale(newLocale) {
+    if (newLocale === locale) return;
+    const newTranslations = await fetchTranslationsFor(newLocale);
+    locale = newLocale;
+    translations = newTranslations;
+}
+
+async function fetchTranslationsFor(newLocale) {
+    const response = await fetch(`/static/lang/${newLocale}.json`);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch translations for ${newLocale}.`);
+    }
+    return await response.json();
+}
+
+function translatePage() {
+    document
+        .querySelectorAll("[data-i18n-key]")
+        .forEach(translateElement);
+}
+
+function translateElement(element) {
+    const key = element.getAttribute("data-i18n-key");
+    const translation = translations[key];
+    element.innerHTML = translation;
+}
