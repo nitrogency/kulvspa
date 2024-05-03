070d7b40dbc10d4dba966de2479c3f93c9707ecb
nitrogency <nitrog@protonmail.com>
2024-05-03 16:39:46 +0300

    Fixed missing 404 upon no post

--- frontend/static/js/views/PostView.js
+++ frontend/static/js/views/PostView.js
@@ -6,9 +6,19 @@ export default class extends AbstractView {
         this.postId = params.id;
     }
 
-    async getHtml() {
+     async getHtml() {
         const response = await fetch(`/static/posts/${this.postId}.html`);
         const html = await response.text();
+        if (html.includes("index")) {
+            return `
+            <body>
+                <div class="flex flex-col items-center justify-start py-8 h-screen">
+                    <h1 data-i18n-key="404_title" class="text-5xl font-extrabold dark:text-white pb-8">The page you're looking for wasn't found!</h1>
+                    <p data-i18n-key="404_p" class="text-lg font-normal lg:text-xl">Click <a href="/" class="nav__link text-yellow-300 hover:underline" data-link>here</a> to go back.</p>
+                </div>  
+            </body>
+            `;
+        }
         return `
             ${html}
         `;
