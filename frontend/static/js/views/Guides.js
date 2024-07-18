106a27f617331d2c57381c8318da496258b2405e
nitrogency <nitrog@protonmail.com>
2024-07-18 19:55:08 +0300

    HTB Footprinting lab post

--- frontend/static/js/views/Guides.js
+++ frontend/static/js/views/Guides.js
@@ -23,6 +23,9 @@ export default class extends AbstractView {
                     <div class="max-w-screen-xl text-center">
                         <p class="text-lg font-normal lg:text-xl"><a data-i18n-key="htb_nmap_title" href="/posts/11" class="nav__link" data-link>HTB - Network Enumeration with Nmap Guide</a></p>
                     </div>
+                    <div class="max-w-screen-xl text-center">
+                        <p class="text-lg font-normal lg:text-xl"><a data-i18n-key="htb_foot_title" href="/posts/12" class="nav__link" data-link>HTB - Footprinting Guide</a></p>
+                    </div>
                 </div>  
             </body>
         `;
